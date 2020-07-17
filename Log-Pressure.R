68dacc5eb0a41ea6d4eab065ce3daf9f1d8a0119
Yuritzi <56103411+Yuritzi15@users.noreply.github.com>
2020-07-16 22:30:55 -0500

    Update Log-Pressure.R

--- Log-Pressure.R
+++ Log-Pressure.R
@@ -18,3 +18,7 @@ datos<-read.csv(ruta)
 #separar registros de las 12 hrs
 datosdoce<-datos %>% filter(hour==12)
  
+
+datosdoce1<- datosdoce %>% select(year,month,dayofweek,dayofmonth,hour,CO,NO,NO2,NOX,O3,PM10,PM2_5,pressure,rainfall,humidity, SO2,solar,temperature,velocity,direction)
+chart.Correlation(datosdoce1, histogram = TRUE, pch="+")
+
